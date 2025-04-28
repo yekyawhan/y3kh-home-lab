f8413cb3508f6836cada62624db2d4d557b3a627
yekyawhan <yekyawhan@gmail.com>
2025-04-28 20:24:11 +0630

    new file:   ProxmoxVE/tools/pve/update-lxcs-cron.sh

---
+++ ProxmoxVE/tools/pve/update-lxcs-cron.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+echo -e "\n $(date)"
+excluded_containers=("$@")
+function update_container() {
+  container=$1
+  name=$(pct exec "$container" hostname)
+  echo -e "\n [Info] Updating $container : $name \n"
+  os=$(pct config "$container" | awk '/^ostype/ {print $2}')
+  case "$os" in
+  alpine) pct exec "$container" -- ash -c "apk update && apk upgrade" ;;
+  archlinux) pct exec "$container" -- bash -c "pacman -Syyu --noconfirm" ;;
+  fedora | rocky | centos | alma) pct exec "$container" -- bash -c "dnf -y update && dnf -y upgrade" ;;
+  ubuntu | debian | devuan) pct exec "$container" -- bash -c "apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confold" dist-upgrade -y; rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED" ;;
+  opensuse) pct exec "$container" -- bash -c "zypper ref && zypper --non-interactive dup" ;;
+  esac
+}
+
+for container in $(pct list | awk '{if(NR>1) print $1}'); do
+  excluded=false
+  for excluded_container in "${excluded_containers[@]}"; do
+    if [ "$container" == "$excluded_container" ]; then
+      excluded=true
+      break
+    fi
+  done
+  if [ "$excluded" == true ]; then
+    echo -e "[Info] Skipping $container"
+    sleep 1
+  else
+    status=$(pct status $container)
+    template=$(pct config $container | grep -q "template:" && echo "true" || echo "false")
+    if [ "$template" == "false" ] && [ "$status" == "status: stopped" ]; then
+      echo -e "[Info] Starting $container"
+      pct start $container
+      sleep 5
+      update_container $container
+      echo -e "[Info] Shutting down $container"
+      pct shutdown $container &
+    elif [ "$status" == "status: running" ]; then
+      update_container $container
+    fi
+  fi
+done
+wait
